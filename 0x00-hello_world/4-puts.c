5b4244b2c828b8aa9f551de5a36d4ab72c086031
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-16 02:38:13 -0800

    Hotfix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 
 /**
  * main - prints ""Programming is like building a multilingual puzzle"
- *
  * Return: 0 always 
  */ 
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
