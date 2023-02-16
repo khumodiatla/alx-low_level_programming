cc00be0e03d99b9e3975c952353b6198b206527a
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-16 02:29:31 -0800

    Add newlines

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
+
 /**
  * main - prints "with proper grammar, but the outcome is a piece of art,, followed by a new line"
- * return: 0 
- */
+ * Return: 0 
+ * /
+ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
