aea9cab928a78343e6698562e9144261bc98f0f1
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-16 03:28:43 -0800

    Hotfix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,6 @@
  * main - prints with proper grammar, but the outcome is a piece of art,
  * Return: 0 always 
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
