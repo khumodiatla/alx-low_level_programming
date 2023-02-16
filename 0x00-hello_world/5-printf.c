8ef9c6d5ec443879bc7965d2e8f015d417e25804
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-16 03:12:29 -0800

    add 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
+ * Return: 0 always 
+ */
+
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
