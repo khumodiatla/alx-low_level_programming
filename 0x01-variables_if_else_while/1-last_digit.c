4f21df0f2ff5aa5a11122a734dcd87c64d2fc87c
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-17 04:40:23 -0800

    Remove if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,8 +16,6 @@ int main(void)
 	n = rand() - RAND_MAX / 2;	
 	last_digit = n % 10;
 
-	if( last_digit < 0)
-		last_digit /= -1;
 	if (last_digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	if (last_digit == 0)
