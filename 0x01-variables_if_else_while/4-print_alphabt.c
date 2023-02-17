b9428b04e648ee70f42344bc001244627af502b0
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-17 05:42:05 -0800

    Fix error

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,7 @@ int main(void)
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		if (c != 'q')
-			putchar(c);
-		if (c != 'e')
+		if (c != 'q' && c != 'e')
                         putchar(c);
 	}
 	putchar('\n');
