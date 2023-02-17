2dc4d8e4cff9f97a10fe193dffbb1ea8c42a8e9b
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-17 07:08:00 -0800

    Betty style

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,19 +11,18 @@ int main(void)
 	for (i = 48; i <= 55; i++)
 	{
 		for (j = i + 1; j <= 56; j++)
-        	{       
+		{       
 			for (k = j + 1; k <= 57; k++)
-        		{       
+			{       
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				
 				if (i != 55)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-        		}
+			}
         	}
 	}
 	putchar('\n');
