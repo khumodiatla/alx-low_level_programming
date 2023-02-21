ff25cdb3e7f6a81d17e50f3b92d033661439bd37
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-20 22:26:58 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_10 - prints all alphabet in lower case
+ */
+
+void print_alphabet_x10()
+{
+	char C = 'a';
+	int i = 1;
+
+	while ( i <= 10)
+	{
+		while (C <= 'z')
+		{
+			_putchar(C);
+			C++;
+		}
+		_putchar('\n');
+	}
+}
