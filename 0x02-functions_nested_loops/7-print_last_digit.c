10564aaa66bafd1d247a757bfda7a67ba9f09df1
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-21 12:45:23 -0800

    Add 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit of n
+ * @n: nuumber to evaluate
+ * Return: last digit of n
+ */
+
+int print_last_digit(int n)
+{
+	return (n%10);
+}
