7fca059a120ca9b0f10af962faa00979efe14fd3
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-21 12:48:01 -0800

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _abs - computes absoute of a value
- * @c: value to compute
+ * _abs - computes abs value of n
+ * @n: value to take abs value of
  * Return: abs value of n
  */
 int _abs(int n)
