52efb3fc8fd558eec84b02530b6cc638ed1dfc81
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-23 12:39:46 -0800

    Add 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - check if char is upper case
+ * @c: char to check
+ * Return: 1 if c is upper cas,0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return isupper(c) ? 1 : 0;
+}
