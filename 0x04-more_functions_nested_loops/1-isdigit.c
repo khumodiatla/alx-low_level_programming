7cc0f0903c0f40b51d6f0b420332b51a19cd7157
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-23 12:46:55 -0800

    Add 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isdigit - checks if c is digit
+ * @c: char to check
+ * Return: 1 if c is a letter otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	return (isdigit(c) ? 1 : 0);
+}
