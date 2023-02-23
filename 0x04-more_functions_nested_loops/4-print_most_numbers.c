a4265d6cd3e9d8982c38905aa6301a4bab15037b
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-23 13:18:22 -0800

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,9 +8,9 @@ void print_numbers(void)
 {
 	int i;
 
-	for(i = 0; i < 10; i++)
+	for(i = 48; i < 58; i++)
 	{
-		if (i != 2 && i != 4)
+		if (i != 49 && i != 51)
 			_putchar(i);
 	}
 	_putchar('\n');
