b27a74e9678f2034e7543352176651a9783ebca8
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-23 13:09:56 -0800

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <unistd.h>
 
 #ifndef _MAIN_H
 #define _MAIN_H
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
