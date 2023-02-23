30bb25f883fb4d7c3dc540896b9f0e660af477f3
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-23 12:55:13 -0800

    Add 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiples a and b
+ * @a: number to multiple
+ * @b: number to multiple
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
