ad74eb86e4bc4e77cc5832330bc58d3d2a5761bf
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-23 13:04:49 -0800

    Add 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: None
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			printf("%d", i);
+	}
+}
