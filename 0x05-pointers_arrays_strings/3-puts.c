4d4357748b882cc7b89a0f0551a9dca57a70dd61
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-27 06:16:15 -0800

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,8 +4,12 @@
  * @s: string to print 
  */
 
-void _puts(char *str)
+void _puts(char *str);
 {
-	printf("%s", *str);
+	int i;
+
+	for(i = 0; i < strlen(str); i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
 
