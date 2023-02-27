807705266615b0e69940fc56fa3976ba636ea503
khumodiatla <khumomoatlhodi@gmail.com>
2023-02-27 06:40:32 -0800

    Add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - print s in reverse
+ * @s: string to print in reverse
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s)-1; i >= 0; i--)
+		putchar(str[i]);
+	putchar('\n');
+}
